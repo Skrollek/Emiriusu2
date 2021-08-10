2ea2983fa7072480119a30960c9755175d867b8c
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-08-10 22:29:59 +0200

    Legacy functions are now static

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,10 +1,10 @@
 #include "engine.hpp"
 
-void error_callback (int error, const char* description) {
+static void error_callback (int error, const char* description) {
     std::cerr << "Error: " << description << '\n';
 }
 
-void key_callback (GLFWwindow* window, int key, int scancode, int action, int mods) {
+static void key_callback (GLFWwindow* window, int key, int scancode, int action, int mods) {
 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose (window, GLFW_TRUE);
